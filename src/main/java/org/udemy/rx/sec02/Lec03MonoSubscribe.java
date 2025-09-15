1b47ae3dfca8d3de8e541b3a0a4d9e9a1031425a
praveenkumar09 <tpkprav@gmail.com>
2025-09-15 22:58:46 +0800

    Added Mono

--- src/main/java/org/udemy/rx/sec02/Lec03MonoSubscribe.java
+++ src/main/java/org/udemy/rx/sec02/Lec03MonoSubscribe.java
@@ -1,5 +1,6 @@
 package org.udemy.rx.sec02;
 
+import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.udemy.rx.sec01.subscriber.SubscriberImpl;
@@ -10,6 +11,11 @@ public class Lec03MonoSubscribe {
     private static final Logger log = LoggerFactory.getLogger(Lec03MonoSubscribe.class);
 
     public static void main(String[] args) {
-
+        Mono<String> mono = Mono.just("Hello World!");
+        mono.subscribe(
+                i -> log.info("Received value : {}",i),
+                err -> log.error("error",err),
+                () -> log.info("completed"),
+                Subscription::cancel);
     }
 }
