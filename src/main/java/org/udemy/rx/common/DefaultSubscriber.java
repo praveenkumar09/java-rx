11feaa5de8368363da052a87c60d664bde425763
praveenkumar09 <tpkprav@gmail.com>
2025-09-15 23:10:52 +0800

    Added common subscriber and util class

---
+++ src/main/java/org/udemy/rx/common/DefaultSubscriber.java
@@ -0,0 +1,37 @@
+package org.udemy.rx.common;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DefaultSubscriber<T> implements Subscriber<T> {
+
+    private static final Logger log = LoggerFactory.getLogger(DefaultSubscriber.class);
+    private String name;
+
+    public DefaultSubscriber(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        log.info("Subscription received");
+        subscription.request(Long.MAX_VALUE);
+    }
+
+    @Override
+    public void onNext(T t) {
+        log.info("Received item: {} ,name : {}",t, this.name);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        log.error("Error occurred: {}, name : {}",throwable.getMessage(),this.name);
+    }
+
+    @Override
+    public void onComplete() {
+        log.info("Completed : name : {}",this.name);
+    }
+}
---
+++ src/main/java/org/udemy/rx/sec02/Demo.java
@@ -0,0 +1,15 @@
+package org.udemy.rx.sec02;
+
+import org.slf4j.Logger;
+import reactor.core.publisher.Mono;
+
+import static org.udemy.rx.common.Util.subscriber;
+
+public class Demo {
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(Demo.class);
+    public static void main(String[] args) {
+        Mono<String> mono = Mono.just("Hello World!");
+        mono.subscribe(subscriber("sub1"));
+        mono.subscribe(subscriber("sub2"));
+    }
+}
