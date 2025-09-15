167520f881cc6a0ccf0c19697dff9a97fdbb9330
praveenkumar09 <tpkprav@gmail.com>
2025-09-15 22:49:00 +0800

    Addeed Mono Jsut examples

---
+++ src/main/java/org/udemy/rx/sec02/Lec03MonoSubscribe.java
@@ -0,0 +1,15 @@
+package org.udemy.rx.sec02;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.udemy.rx.sec01.subscriber.SubscriberImpl;
+import reactor.core.publisher.Mono;
+
+public class Lec03MonoSubscribe {
+
+    private static final Logger log = LoggerFactory.getLogger(Lec03MonoSubscribe.class);
+
+    public static void main(String[] args) {
+
+    }
+}
