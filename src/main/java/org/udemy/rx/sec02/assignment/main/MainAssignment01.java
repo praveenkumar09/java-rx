517f78fd7dc63cabedafc0824eceff511d63e26d
praveenkumar09 <tpkprav@gmail.com>
2025-09-18 21:51:24 +0800

    sec02 assignment added:]

---
+++ src/main/java/org/udemy/rx/sec02/assignment/main/MainAssignment01.java
@@ -0,0 +1,27 @@
+package org.udemy.rx.sec02.assignment.main;
+
+import org.reactivestreams.Subscription;
+import org.slf4j.Logger;
+import org.udemy.rx.common.Util;
+import org.udemy.rx.sec02.assignment.impl.FileServiceImpl;
+
+public class MainAssignment01 {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(MainAssignment01.class);
+
+    public static void main(String[] args) {
+        new FileServiceImpl()
+                .read("/Users/praveenkumarthangaraj/Documents/JavaCourse/ReactiveProgramming/java-rx/src/main/java/org/udemy/rx/sec02/assignment/resources/Readfile.txt").subscribe(
+                        i -> log.info("Received value : {}",i),
+                        err -> log.error("error",err),
+                        () -> log.info("completed"));
+        Util.sleep(10);
+
+        new FileServiceImpl()
+                .read("/Users/Documents/JavaCourse/ReactiveProgramming/java-rx/src/main/java/org/udemy/rx/sec02/assignment/resources/Readfile.txt").subscribe(
+                        i -> log.info("Received value : {}",i),
+                        err -> log.error("error",err),
+                        () -> log.info("completed"));
+        Util.sleep(10);
+    }
+}
