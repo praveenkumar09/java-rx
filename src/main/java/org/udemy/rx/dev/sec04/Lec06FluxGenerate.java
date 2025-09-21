6f3f0b3ab1e7e1f6a117673514acafc2eaf61041
praveenkumar09 <tpkprav@gmail.com>
2025-09-21 21:46:55 +0800

    Added example for flux generate

---
+++ src/main/java/org/udemy/rx/dev/sec04/Lec06FluxGenerate.java
@@ -0,0 +1,24 @@
+package org.udemy.rx.dev.sec04;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.udemy.rx.common.Util;
+import reactor.core.publisher.Flux;
+
+public class Lec06FluxGenerate {
+    private static final Logger log = LoggerFactory.getLogger(Lec06FluxGenerate.class);
+
+    public static void main(String[] args) {
+        log.info("Main method started");
+        Flux.generate(sink -> {
+            String countryName = Util.faker().country().name();
+            if(countryName.equalsIgnoreCase("canada")) {
+                sink.error(new RuntimeException("Canada received"));
+                sink.complete();
+            }else {
+                sink.next(countryName);
+            }
+        })
+                .subscribe(Util.subscriber("subscriber-1"));
+    }
+}
