a03d0f665a517e055b8d36cf05c43fd52bf48adb
praveenkumar09 <tpkprav@gmail.com>
2025-10-14 15:21:57 +0800

    Added example for expectNextMatches()

--- src/test/java/com/udemy/rx/Lec04RangeTest.java
+++ src/test/java/com/udemy/rx/Lec04RangeTest.java
@@ -3,6 +3,7 @@ package com.udemy.rx;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.udemy.rx.common.Util;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -14,12 +15,28 @@ public class Lec04RangeTest {
                 .log();
     }
 
+    private Flux<Integer> getRandomNumbers(){
+        return Flux.range(1,10)
+                .map(i -> Util.faker().random().nextInt(1,10))
+                .log();
+    }
+
     @Test
     public void getItemsTestWithNextCount(){
         StepVerifier
                 .create(getItems())
                 .expectNext(1,2,3)
                 .expectNextCount(7)
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void getItemsTestWithRandomNumbers(){
+        StepVerifier
+                .create(getRandomNumbers())
+                .expectNextMatches(i -> i >= 1 && i <= 10)
+                .expectNextCount(9)
                 .expectComplete()
                 .verify();
     }
