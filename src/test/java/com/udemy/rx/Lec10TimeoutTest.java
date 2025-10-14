42733f5b768691053bc8691b98a43fa50c8de3c1
praveenkumar09 <tpkprav@gmail.com>
2025-10-14 17:59:26 +0800

    Added example for timeouttest()

---
+++ src/test/java/com/udemy/rx/Lec10TimeoutTest.java
@@ -0,0 +1,25 @@
+package com.udemy.rx;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+
+public class Lec10TimeoutTest {
+
+    private Flux<Integer> getItems() {
+        return Flux
+                .range(1, 5)
+                .delayElements(Duration.ofMillis(200));
+    }
+
+    @Test
+    public void _test_timeout(){
+        StepVerifier
+                .create(getItems())
+                .expectNextCount(5)
+                .expectComplete()
+                .verify(Duration.ofMillis(500));
+    }
+}
